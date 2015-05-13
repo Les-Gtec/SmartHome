d1b118ae585478c93208b2c993074d5da830d06e
Les Mahon <les@gingertec.com>
2015-05-13 11:45:17 +0100

    Reduced to 2 meter counter on pins 0 and 2

--- HardwareCoreBased/WaterMeterCounter.ino
+++ HardwareCoreBased/WaterMeterCounter.ino
@@ -1,26 +1,26 @@
-#define ARRAY_SIZE 3
+#define ARRAY_SIZE 2
 // array of pins to be debounced
 short pinsToDebounce[]={
-  0,2,4
+  0,2
 };
 // array of pin state
 int swStates[]={
-  0,0,0};
+  0,0};
 // array of previous pin state
 int swPrevStates[]={
-  0,0,0};
+  0,0};
 // array to store the actual state during debounce
 int swDebouncedStates[]={
-  0,0,0};
+  0,0};
 // array to store the previous state during debounce
-int swPrevDebounceStates[]={0,0,0};
+int swPrevDebounceStates[]={0,0};
 // time to debounce
-int debounceDelay=100;
+int debounceDelay=250;
 // array of previous times the pin has been checked
 long prevTimes[]={
-  0,0,0};
+  0,0};
 
-int meterCounts[]={0,0,0};
+int meterCounts[]={0,0};
 
 bool firstPass = true;
 
@@ -38,7 +38,7 @@ void setup()
     //Set up spark variables to read counter
     Spark.variable("count1", &meterCounts[0], INT);
     Spark.variable("count2", &meterCounts[1], INT);
-    Spark.variable("count3", &meterCounts[2], INT);
+    //Spark.variable("count3", &meterCounts[2], INT);
     Spark.function("pushCounts", pushCounts);
     initSwitches();
 }
@@ -101,12 +101,6 @@ void checkStateChange(){
   for(short sw=0;sw<ARRAY_SIZE;sw++){
     if(swPrevDebounceStates[sw]!=swDebouncedStates[sw]){
 
-      Serial.println("");
-      Serial.print("button ");
-      Serial.print(sw);
-      Serial.print(" ");
-      Serial.println(swDebouncedStates[sw]);
-
       if(swDebouncedStates[sw]==1){
         pinActive(sw);
       }
@@ -119,57 +113,45 @@ void checkStateChange(){
 }
 
 void printDebStates(){
-  Serial.println("%%%%%%%%%%%%%%%%%%%%%%%%");
+
   for(int i=0;i<ARRAY_SIZE;i++){
-    Serial.print(swDebouncedStates[i]);
-    Serial.print('*');
 
-  }
+  };
 
-  Serial.println("");
 }
 void pinActive(int _id){
 
-  Serial.print("active pin ");
-  Serial.println(pinsToDebounce[_id]);
   if(!firstPass){
     meterCounts[_id]++;
   }
 }
+
 void pinInactive(int _id){
-   Serial.print("inactive pin ");
- Serial.println(pinsToDebounce[_id]);
+
 }
 
 int pushCounts(String command)
 {
-    for(short sw=0;sw<ARRAY_SIZE;sw++){
-      meterCounts[sw] = (meterCounts[sw]-1)*10;
-    }
-     ThingSpeakUpdate("field1="+String(meterCounts[0])+"&field2="+String(meterCounts[1])+"&field3="+String(meterCounts[2]));
-     for(short sw=0;sw<ARRAY_SIZE;sw++){
-       meterCounts[sw] = 1;
-     }
+    //for(short sw=0;sw<ARRAY_SIZE;sw++){
+    //  meterCounts[sw] = (meterCounts[sw]-1)*10;
+    //}
+     ThingSpeakUpdate("field1="+String(meterCounts[0])+"&field2="+String(meterCounts[1])+"&field3=0");
+     //for(short sw=0;sw<ARRAY_SIZE;sw++){
+//       meterCounts[sw] = 1;
+     //}
      return 200;
-
 }
 
 /*------------------------------------------------
-Sends sensor data to Thingspeak
+Sends Meter Counts to Thingspeak
 Inputs: String, data to be entered for each field
 Returns:
 ------------------------------------------------*/
 void ThingSpeakUpdate(String tsData)
 {
-    Serial.println("Date string: " + tsData);
-
-    Serial.println("...Connecting to Thingspeak");
-
-    // Connecting and sending data to Thingspeak
+      // Connecting and sending data to Thingspeak
     if(client.connect("api.thingspeak.com", 80))
     {
-        Serial.println("...Connection succesful, updating datastreams");
-
         client.print("POST /update HTTP/1.1\n");
         client.print("Host: api.thingspeak.com\n");
         client.print("Connection: close\n");
@@ -183,12 +165,9 @@ void ThingSpeakUpdate(String tsData)
 
         // This delay is pivitol without it the TCP client will often close before the data is fully sent
         delay(200);
-
-        Serial.println("Thingspeak update sent.");
     }
     else{
         // Failed to connect to Thingspeak
-        Serial.println("Unable to connect to Thingspeak.");
     }
 
     if(!client.connected()){
